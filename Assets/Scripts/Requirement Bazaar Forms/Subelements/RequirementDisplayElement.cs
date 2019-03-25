bff07607d7e7fa8195b8ed3f991f67250ff9e960
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2019-03-25 19:04:59 +0100

    started on UI voting functionality

--- Assets/Scripts/Requirement Bazaar Forms/Subelements/RequirementDisplayElement.cs
+++ Assets/Scripts/Requirement Bazaar Forms/Subelements/RequirementDisplayElement.cs
@@ -1,4 +1,5 @@
-﻿using Org.Requirements_Bazaar.Common;
+﻿using Org.Requirements_Bazaar.API;
+using Org.Requirements_Bazaar.Common;
 using Org.Requirements_Bazaar.DataModel;
 using System;
 using System.Collections;
@@ -14,6 +15,10 @@ namespace Org.Requirements_Bazaar.AR_VR_Forms
         [SerializeField] private Text titleLabel;
         [SerializeField] private Text descriptionLabel;
         [SerializeField] private Text votesLabel;
+        [SerializeField] private Button voteButton;
+        [SerializeField] private Image voteImage;
+        [SerializeField] private Sprite notVotedSprite;
+        [SerializeField] private Sprite votedSprite;
 
         private Requirement requirement;
 
@@ -39,6 +44,14 @@ namespace Org.Requirements_Bazaar.AR_VR_Forms
             titleLabel.text = requirement.Name;
             descriptionLabel.text = requirement.Description;
             votesLabel.text = requirement.UpVotes.ToString();
+            //if (requirement.UserVoted)
+            //{
+            //    voteImage.sprite = votedSprite;
+            //}
+            //else
+            //{
+            //    voteImage.sprite = notVotedSprite;
+            //}
         }
 
         public void OnClick()
@@ -51,6 +64,20 @@ namespace Org.Requirements_Bazaar.AR_VR_Forms
             createEditForm.PageClosed += OnInstantiatedPageClosed;
 
             canvas.gameObject.SetActive(false);
+        }
+
+        public async void OnVoteClick()
+        {
+            voteButton.interactable = false;
+            //if (requirement.UserVoted) // if user already voted => undo by voting down
+            //{
+            //    Requirement = await RequirementsBazaar.VoteForRequirement(Requirement.Id, VotingDirection.DOWN);
+            //}
+            //else // if user did not yet vote => vote up
+            //{
+            //    Requirement = await RequirementsBazaar.VoteForRequirement(Requirement.Id);
+            //}
+            voteButton.interactable = true;
         }
 
         private void OnInstantiatedPageClosed(object sender, EventArgs e)
